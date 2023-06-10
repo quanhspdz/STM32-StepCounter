d3ad7235e9d0786a7d2349ffe09c8dbf55f6b14f
quanhspdz <pc.quanhdz@gmail.com>
2023-06-10 13:56:58 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,340 @@
+#include "i2c-lcd.h"
+#include "math.h"
+#include "main.h"
+
+I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_I2C2_Init(void);
+
+#define MPU6050_ADDR 0xD0
+
+
+#define SMPLRT_DIV_REG 0x19
+#define GYRO_CONFIG_REG 0x1B
+#define ACCEL_CONFIG_REG 0x1C
+#define ACCEL_XOUT_H_REG 0x3B
+#define TEMP_OUT_H_REG 0x41
+#define GYRO_XOUT_H_REG 0x43
+#define PWR_MGMT_1_REG 0x6B
+#define WHO_AM_I_REG 0x75
+
+#define THRESHOLD 0.3 // Ngu?ng phát hi?n bu?c chân
+
+
+int16_t Accel_X_RAW = 0;
+int16_t Accel_Y_RAW = 0;
+int16_t Accel_Z_RAW = 0;
+
+int16_t Gyro_X_RAW = 0;
+int16_t Gyro_Y_RAW = 0;
+int16_t Gyro_Z_RAW = 0;
+
+float Ax, Ay, Az, Gx, Gy, Gz;
+
+
+void MPU6050_Init (void)
+{
+	uint8_t check;
+	uint8_t Data;
+
+	// check device ID WHO_AM_I
+
+	HAL_I2C_Mem_Read (&hi2c1, MPU6050_ADDR,WHO_AM_I_REG,1, &check, 1, 1000);
+
+	if (check == 104)  // 0x68 will be returned by the sensor if everything goes well
+	{
+		// power management register 0X6B we should write all 0's to wake the sensor up
+		Data = 0;
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, PWR_MGMT_1_REG, 1,&Data, 1, 1000);
+
+		// Set DATA RATE of 1KHz by writing SMPLRT_DIV register
+		Data = 0x07;
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &Data, 1, 1000);
+
+		// Set accelerometer configuration in ACCEL_CONFIG Register
+		// XA_ST=0,YA_ST=0,ZA_ST=0, FS_SEL=0 -> ? 2g
+		Data = 0x00;
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &Data, 1, 1000);
+
+		// Set Gyroscopic configuration in GYRO_CONFIG Register
+		// XG_ST=0,YG_ST=0,ZG_ST=0, FS_SEL=0 -> ? 250 ?/s
+		Data = 0x00;
+		HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &Data, 1, 1000);
+	}
+
+}
+
+
+void MPU6050_Read_Accel (void)
+{
+	uint8_t Rec_Data[6];
+
+	// Read 6 BYTES of data starting from ACCEL_XOUT_H register
+
+	HAL_I2C_Mem_Read (&hi2c1, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, 1000);
+
+	Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
+	Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
+	Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
+
+	/*** convert the RAW values into acceleration in 'g'
+	     we have to divide according to the Full scale value set in FS_SEL
+	     I have configured FS_SEL = 0. So I am dividing by 16384.0
+	     for more details check ACCEL_CONFIG Register              ****/
+
+	Ax = Accel_X_RAW/16384.0;
+	Ay = Accel_Y_RAW/16384.0;
+	Az = Accel_Z_RAW/16384.0;
+}
+
+
+void MPU6050_Read_Gyro (void)
+{
+	uint8_t Rec_Data[6];
+
+	// Read 6 BYTES of data starting from GYRO_XOUT_H register
+
+	HAL_I2C_Mem_Read (&hi2c1, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data, 6, 1000);
+
+	Gyro_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
+	Gyro_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
+	Gyro_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
+
+
+	Gx = Gyro_X_RAW/131.0;
+	Gy = Gyro_Y_RAW/131.0;
+	Gz = Gyro_Z_RAW/131.0;
+}
+
+
+int main(void)
+{
+	char buf[4];
+
+	HAL_Init();
+	SystemClock_Config();
+	MX_GPIO_Init();
+	MX_I2C1_Init();
+	MX_I2C2_Init();
+
+	lcd_init();
+	MPU6050_Init();
+	lcd_send_string("initialized");
+	HAL_Delay(1000);  // wait for 1 sec
+	lcd_clear();
+
+	// Kh?i t?o bi?n d?m bu?c chân
+	int stepCount = 0;
+	int pauseRequest = 0;
+	int stepDetected = 0;
+	float currentVector, previousVector = 0, diffVector = 0;
+	float thresholdVector = 0.19;
+	float delayTime = 100;
+	
+	// Kh?i t?o c?u hình GPIO
+	GPIO_InitTypeDef GPIO_InitStruct;
+	
+	// B?t Clock cho GPIOA
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	// C?u hình chân PA0 và PA1 là input
+	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLUP; // S? d?ng pull-up resistor
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	
+	// B?t Clock cho GPIOC
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  
+  // C?u hình chân PC13 là output
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+	
+	// C?u hình SysTick timer
+	SysTick_Config(SystemCoreClock / 2); // T?n s? SysTick là 1Hz (1 giây)
+
+	while (1)
+	{
+		// Ð?c tr?ng thái t? switch 1
+		uint8_t switch1_state = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
+
+		// Ð?c tr?ng thái t? switch 2
+		uint8_t switch2_state = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1);
+
+		if (switch1_state == GPIO_PIN_RESET) //sw1 là pause|resume
+		{
+			// T?m d?ng ho?c ti?p t?c d?m bu?c chân
+			HAL_Delay(50); // delay tranh nhieu
+			if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == GPIO_PIN_RESET)
+			{
+				pauseRequest = !pauseRequest; // Chuy?n d?i tr?ng thái t?m d?ng ho?c ti?p t?c
+				lcd_clear();
+			}
+		}
+
+		if (switch2_state == GPIO_PIN_RESET) //sw2 là reset stepCount
+		{
+			// Reset bi?n stepCount v? 0
+			stepCount = 0;
+			lcd_clear();
+		}
+		
+		if (pauseRequest) {
+			//tat nhay den xanh chan pc13
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+			
+			//in ra man hinh thong bao tam dung
+			lcd_send_cmd(0x80 | 0x00);  // goto 1,1
+			lcd_send_string("Please press SW1");
+			
+			lcd_send_cmd(0x80 | 0x40);  // goto 2,1
+			lcd_send_string("to continue!");
+		} else {
+			// Nháy LED chân PC13 v?i t?n s? 1Hz
+			static uint32_t previousTick = 0;
+			uint32_t currentTick = HAL_GetTick();
+
+			if (currentTick - previousTick >= 500)
+			{
+				previousTick = currentTick;
+				HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+			}
+			
+			// read the Accelerometer and Gyro values
+			MPU6050_Read_Accel();
+			MPU6050_Read_Gyro();
+
+			//Code d?m bu?c chân
+
+			currentVector = sqrt(Ax * Ax + Ay * Ay + Az * Az);
+			diffVector = fabs(currentVector - previousVector);
+
+			// Ki?m tra n?u góc theta vu?t quá ngu?ng threshold
+			// N?u tang bình phuong gia t?c vu?t quá ngu?ng và chua phát hi?n bu?c chân tru?c dó
+			if (diffVector > thresholdVector && previousVector && !stepDetected)
+			{
+				stepCount++; // Tang bi?n d?m s? bu?c chân lên 1
+			}
+			else if (diffVector <= thresholdVector)
+			{
+				stepDetected = 0;
+			}
+			
+			// G?i l?nh clear screen d?n LCD
+
+			lcd_send_cmd(0x80 | 0x00);  // goto 1,1
+			lcd_send_string("StepCount: ");
+			sprintf(buf, "%d", stepCount);
+			lcd_send_string(buf);
+			lcd_send_string("");
+
+			lcd_send_cmd(0x80 | 0x40);  // goto 2,1
+
+			sprintf(buf, "%.3f", diffVector);
+			lcd_send_string(buf);
+
+			previousVector = currentVector;
+			HAL_Delay(delayTime);  // wait for a while
+		}
+	}
+}
+
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_I2C1_Init(void)
+{
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+static void MX_I2C2_Init(void)
+{
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
+
+void Error_Handler(void)
+{
+}
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+
+}
+#endif
